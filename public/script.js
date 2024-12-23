b39b1dcf1003e098834d9b6628a180bf998609ec
Glitch (glitch-hello-node) <none>
2024-12-23 08:22:29 +0000

    🕴🐗 Checkpoint ./public/script.js:70209477/1642

--- public/script.js
+++ public/script.js
@@ -13,6 +13,27 @@ function starsaccess {
   let star = document.getElementbyClassName("stars");
   let output = document.getElementbyId = ("outputs");
   
+  let number;
+  for(i = 0; i < n; i++) {
+    if (n>0) {
+      (n == 1) number = "one";
+    }
+      else {(n == 2) number = "two";
+              }
+      else { (n == 3) number = "three";
+             }
+      else {(n == 4) number = "four";
+              }
+      else {(n == 5) number = "five";
+             }
+     star[i].className = "star " + number;
+    }
+      output.innerText = "Rating is: " + n + "/5";
+  }
+    
   
+  
+     
+ 
   
 }
